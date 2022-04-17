a77362c422758a3d6a3879d892650db0f162b4df
theDapperFoxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2022-04-17 13:22:03 -0400

    experimenting with loop (commented out)

--- script/script.js
+++ script/script.js
@@ -16,12 +16,25 @@ document.querySelector('input').addEventListener('keypress', function(e) {
     .then(data => {
       document.querySelector("#drinkList").innerHTML = ""
       data.drinks.forEach(function(singleDrink) {
+        // console.log(singleDrink)
       const hr = document.createElement('hr')
       const h2 = document.createElement('h2')
       h2.innerText = singleDrink.strDrink
 
       const img = document.createElement('img')
       img.src = singleDrink.strDrinkThumb
+
+      // for (let x = 1; x <= 15; x++) {
+      //   if (singleDrink.strIngredient1 === null || singleDrink.strIngredient1 === "") {
+      //     continue;
+      //   } else {
+      //   let ingredient = 'strIngredient' + x
+      //   return ingredient
+      // }
+      // }
+
+        // const h4 = document.createElement('h4')
+        // h4.innerText = ingredient.value
 
       const h3 = document.createElement('h3')
       h3.innerText = singleDrink.strInstructions
@@ -33,6 +46,7 @@ document.querySelector('input').addEventListener('keypress', function(e) {
         div.appendChild(h2)
         div.appendChild(img)
         div.appendChild(h3)
+        // div.appendChild(h4)
 
         document.querySelector('#drinkList').appendChild(div)
 
